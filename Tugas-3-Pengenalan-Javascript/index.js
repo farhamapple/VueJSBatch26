c768872223dad3bd0dac65bffc578994d54555b8
Farham Harvianto <lipi.farham@gmail.com>
2021-07-15 07:32:06 +0700

    Feat: Tugas-3-Pengenalan-JS

---
+++ Tugas-3-Pengenalan-Javascript/index.js
@@ -0,0 +1,41 @@
+
+console.log('--- Soal No 1----');
+var pertama = "saya sangat senang hari ini";
+var kedua = "belajar javascript itu keren";
+console.log(pertama.concat(kedua));
+
+console.log('');
+console.log('--- Soal No 2----');
+var kataPertama = "10";
+var kataKedua = "2";
+var kataKetiga = "4";
+var kataKeempat = "6";
+
+var strInt_1 = parseInt(kataPertama);  
+var strInt_2 = parseInt(kataKedua); 
+var strInt_3 = parseInt(kataKetiga); 
+var strInt_4 = parseInt(kataKeempat);
+
+var hasil = (strInt_2 * strInt_3) + (strInt_1 + strInt_4);
+console.log(hasil); 
+
+console.log('');
+console.log('--- Soal No 3----');
+var kalimat = 'wah javascript itu keren sekali'; 
+
+var kataPertama = kalimat.substring(0, 3); 
+var kataKedua = kalimat.substring(4, 14); // do your own! 
+var kataKetiga = kalimat.substring(15, 18); // do your own! 
+var kataKeempat = kalimat.substring(19, 24); // do your own! 
+var kataKelima= kalimat.substring(25, 33); // do your own! 
+
+console.log('Kata Pertama: ' + kataPertama); 
+console.log('Kata Kedua: ' + kataKedua); 
+console.log('Kata Ketiga: ' + kataKetiga); 
+console.log('Kata Keempat: ' + kataKeempat); 
+console.log('Kata Kelima: ' + kataKelima);
+
+console.log('');
+console.log('--- Soal No 4----');
+
+
